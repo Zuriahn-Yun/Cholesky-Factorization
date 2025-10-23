9c6239d9eed18f6e204bbab447e4dc8391564d33
Zuriahn-Yun <Zuriahnyun1@gmail.com>
2025-10-22 21:30:35 -0700

    Establish Test Cases Next, Implementation is Done

--- cholesky.py
+++ cholesky.py
@@ -3,49 +3,64 @@ import math
 
 # Generate Random nxn matrix
 def generate_random_spd(n):
+    # Create Random n x n Matrix 
     A = np.random.rand(n,n)
     spd = np.dot(A,A.T)
     spd += np.eye(n) * 1e-6
     return spd
-    
+
+# Generate Random Matrix for Testing
+def generate_random(n):
+    return np.random.rand(n,n)
+
+# Check if Matrix is Symmetric
 def check_symmetric(A):
     A = np.array(A)
-    print(A.T)
     if np.array_equal(A,A.T):
         return True
     return False
         
-# Check if a matrix is Symmetric and Positive Definite
+# Check if a matrix is Symmetric and Positive Definite using Cholesky
 def check_spd(A):
+    # If symmetric try Cholesky
     if check_symmetric(A):
-        # Check for Positive Definite xTAx > 0, unless x = 0, or all EigenValues are greater than 0
-        eigenvalues = np.linalg.eigvals(A)
-        if np.all(eigenvalues > 0):
+        try:
+            cholesky(A)
             return True
-        else:
+        except:
+            # Return False if Cholesky returns an exception
             return False
+    # Return False if matrix is not Symmetric
     return False
 
 # Cholesky Decomposition
 def cholesky(A):
     try:
+        # Iterate through rows i and columns j
         for i in range(A.shape[0]):
             for j in range(A.shape[1]):
                 summation = 0
+                print(A)
+                # Compute Diagonals
                 if i == j:
                     for k in range(j):
                         summation +=  A[j,k] ** 2
                     A[j,j] = math.sqrt(A[j,j] - summation)  
+                # Compute Lower Triangle Values
                 elif i > j:
                     for k in range(j):
                         summation += A[i,k] * A[j,k]
                     A[i,j] = 1/A[j,j] * (A[i,j] - summation)
+                # Otherwise the values are 0
                 else:
                     A[i,j] = 0
+        print("HERE")
         return A
+    # Exception if matrix is not SPD
     except Exception as e:
         return print("This is not an SPD Matrix")
-    
+
+# Calculate determinant of Cholesky Matrix CHECK THIS ---------------------------------------------------
 def cholesky_det(A):
     curr = 1
     L = cholesky(A)
@@ -53,40 +68,61 @@ def cholesky_det(A):
         curr += curr * L[i,i]
     return curr ** 2
 
-
-def cholesky_solve_forward(A,b):
+# Forward Substitution L * y = b
+def cholesky_forward_sub(A,b):
     L = cholesky(A)
     # Ly = b
     y = np.array([])
     # Compute y 
-    for i in range(len(b.size[0])):
-        print(i)
+    for i in range(len(b)):
         if i == 0:
-            np.append(y,b[0])
+            y = np.append(y,b[i] / L[i,i])
         else:
-            # This is worng
-            np.append(y,b[i] - np.dot(L[i],y) / L[i,i])
-    # Compute x 
+            # Since we are appending to y as we go all of y is already computed
+            # We can dot y with every L value in this row up to but not including i 
+            computed = L[i,:i] @ y
+            y_i = (b[i] - computed) / L[i,i]
+            y = np.append(y,y_i)
+    # Reshape y to a Column Array
+    return y.reshape(-1,1)
+
+# Backwards Substitution, L.T * x = y
+def cholesk_backward_sub(L,y):
     x = np.array([])
-    return x
-
-
-def cholesk_solve_backward(A,x,b):
-    return
+    # L.T * x = y
+    # Compute x, same logic as forward substitution
+    for i in range(len(y)):
+        if i == 0:
+            x = np.append(x,y[i] / L.T[i,i])
+        else:
+            computed = L.T[i,:i] @ x
+            x_i = (y[i] - computed) / L.T[i,i]
+            x = np.append(x,x_i)
+    # Reshape x to column array
+    return x.reshape(-1,1)
 
 if __name__ == "__main__":
-    print("START TEST")
-    A = generate_random_spd(4)
-    Original = A.copy()
+    # print("START TEST")
+    # A = generate_random_spd(4)
+    # Original = A.copy()
     #A = np.matrix([[2,1],[1,2]],dtype=np.float64)
     #print(A)
-    A = cholesky(A)
-    decomp_check = np.matmul(A,A.T)
-    print("L Decomp")
-    print(A)
-    print("CHOLESKY ")
-    print(decomp_check)
-    print("ORIGINAL")
-    print(Original)
-    print(np.allclose(decomp_check,Original))
-    
+    # A = cholesky(A)
+    # decomp_check = np.matmul(A,A.T)
+    # print("L Decomp")
+    # print(A)
+    # print("CHOLESKY ")
+    # print(decomp_check)
+    # print("ORIGINAL")
+    # print(Original)
+    # print(np.allclose(decomp_check,Original))
+    # Testing Forward Test----------------------------
+    print("Testing Forward")
+    A = generate_random_spd(3)
+    Original_A = A.copy()
+    b = np.array([1,1,1])
+    b = b.reshape(-1,1)
+    y = cholesky_forward_sub(A,b)
+    x = cholesk_backward_sub(A,y)
+    print(x)
+    print(np.matmul(Original_A,x))
--- problems.py
+++ problems.py
@@ -1,7 +1,8 @@
 import cholesky
 import numpy as np
 
-# 1 
+# 1 --------------------------------------------
+# Generate SPD Pascal Matrix
 def create_spd_pascal(p,n):
     A = np.zeros((n,n))
     for i in range(n):
@@ -9,22 +10,96 @@ def create_spd_pascal(p,n):
             if i == 0:
                 A[i,j] = p
             elif j == 0:
-                print("HEre")
                 A[i,j] = p 
             else:
                 A[i,j] = A[i,j-1] + A[i-1,j]
     return A
 
+# Generate Determinant of SPD Pascal
 def determinant_spd_pascal(A):
     curr = 1
-    L = cholesky(A)
-    for i in range(L.size[0]):
-        curr += curr * L[i,i]
-    return curr ** 2
+    L = cholesky.cholesky(A)
+    L = L * L.T
+    for i in range(len(L)):
+        curr  = curr * L[i,i]
+    return curr
 
-# 2
+# 2 --------------------------------------------
+# the inverse of an SPD is also SPD, compute the iverses B of the Matrices in question 1 --------------- CHECK THIS Is it okay to use built in inverse function
+def inverse_spd_pascal(A):
+    inv_A = np.linalg.inv(A)
+    # Preseve Symmetry
+    B = 1/2 * (inv_A + inv_A.T)
+    return B
 
+# 3 --------------------------------------------
+# any solution x of the least squares problem
+# B.T * B * x = B.T * b
+# When r is one million this always breaks??--------------------------
+def least_squares(r):
+    B = np.array([[0,2,0],
+                  [r,r,0],
+                  [r,0,r],
+                  [0,1,1]])
+    
+    b = np.array([2,2 * r,2 * r,2])
+    # Reshape to column array
+    b = b.reshape(-1,1)
+    BT_B = np.matmul(B.T,B)
+    BT_b = np.matmul(B.T,b)
+    # BT_B is SPD so we can use Cholesky Factor
+    # B.T * B* x = B.T * b -> LL.Tx = B.T * b
+    # Therefore Ly = B.T * b using forward substitution
+    y = cholesky.cholesky_forward_sub(BT_B,BT_b)
+    x = cholesky.cholesk_backward_sub(BT_B,y)
+    return x
 
+# Numpy doesnt have an exact Solve with Least Squares Function
+# Here is an implementation using QR Factorization
+def QR_least_squares(r):
+    B = np.array([[0,2,0],
+                  [r,r,0],
+                  [r,0,r],
+                  [0,1,1]])
+    b = np.array([2,2 * r,2 * r,2])
+    b = b.reshape(-1,1)
+    Q,R = np.linalg.qr(B)
+    p = Q.T @ b
+    x = np.linalg.solve(R,p)
+    return x
+# 4 -------------------------------------------- ask about the contents of the given matrix 
+def question_4(n):
+    A = np.zeros((n,n))
+    for i in range(n-1):
+        A[i,i] = 2
+        A[i,i + 1] = - 1
+        A[i+1,i] = -1
+    A[0, n - 1] = 1
+    A[n - 1,0] = 1
+    A[n - 1,n - 1] = 2
+    return A
 if __name__ == "__main__":
-    A = create_spd_pascal(5,3)
-    print(A)
+    # A = create_spd_pascal(15,3)
+    # Original_A = A.copy()
+    # det_A = determinant_spd_pascal(A)
+    # print(A)
+    # L = A
+    # print(L * L.T)
+    # print(det_A)
+    
+    # B = inverse_spd_pascal(Original_A)
+    # print(B)
+    # B_cholesky = cholesky.cholesky(B)
+    # print(B_cholesky)
+    # print(B_cholesky * B_cholesky.T)
+    
+    # r = 100000
+    # x_qr = QR_least_squares(r)
+    # x_cholesky = least_squares(r)
+    
+    # print(x_cholesky)
+    # print(x_qr)
+    
+    A = question_4(5)
+    print(cholesky.check_symmetric(A))
+    print(cholesky.check_spd(A))
